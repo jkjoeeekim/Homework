8e642e16c626abbb0e991ea27e97c168f2e30307
Joseph Kim <jkjoeeekim@live.com>
2021-03-15 20:33:21 -0700

    Finish error_handling_function

--- w4d2/error_handling_funtime/super_useful.rb
+++ w4d2/error_handling_funtime/super_useful.rb
@@ -5,12 +5,19 @@ end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
+class CoffeeError < StandardError
+end
+
+class NoFruitError < StandardError
+end
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise NoFruitError
   end 
 end
 
@@ -19,19 +26,35 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  reaction(maybe_fruit)
 end  
 
 # PHASE 4
+class NoYouAintError < StandardError
+end
+
+class WhatsYourNameError < StandardError
+end
+
+class WhatsYourFavPastimeError < StandardError
+end
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    if name.empty?
+      raise WhatsYourNameError
+    elsif yrs_known < 5
+      raise NoYouAintError
+    elsif fav_pastime.empty?
+      raise WhatsYourFavPastimeError
+    end
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known}."
   end
 
   def do_friendstuff
--- w4d2/error_handling_funtime/user_script.rb
+++ w4d2/error_handling_funtime/user_script.rb
@@ -1,11 +1,34 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
+# puts "'5' == #{convert_to_int('5')}"
 
-feed_me_a_fruit
+begin
+    feed_me_a_fruit
+rescue CoffeeError => e
+    puts "You gave me coffee, but try again. #{e.message}" + "\n"
+    retry
+rescue NoFruitError => e
+    puts "You can't try again because you didn't give me any fruits nor coffee. :(  #{e.message}" + "\n"
+end
 
-sam = BestFriend.new('', 1, '')
-
-sam.talk_about_friendship
-sam.do_friendstuff
-sam.give_friendship_bracelet
+begin
+    puts "Whats your name?"
+    name = gets.chomp
+    puts "How long have you known me?"
+    years = convert_to_int(gets.chomp)
+    puts "Whats your hobbies?"
+    hobbies = gets.chomp
+    sam = BestFriend.new(name, years, hobbies)
+    sam.talk_about_friendship
+    sam.do_friendstuff
+    sam.give_friendship_bracelet
+rescue WhatsYourNameError => e
+    puts "What was your name? #{e.message}" + "\n"
+    retry
+rescue NoYouAintError => e
+    puts "I don't know you like that. #{e.message}" + "\n"
+    retry
+rescue WhatsYourFavPastimeError => e
+    puts "What do you like to do? #{e.message}" + "\n"
+    retry
+end
